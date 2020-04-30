537fdc03d331cfcfedc99101666df0de12a321b9
Becky-B <bettsrebecca@yahoo.com>
2020-04-29 18:28:36 -0700

    Products page added

---
+++ Models/ProductPages.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace Truckette.Models
+{
+    public class ProductPages
+    {
+        public Product Product { get; set; }
+        public List<Product> ListOfProducts { get; set; }
+        public List<Category> ListOfCategories { get; set; }
+        public FilterForm FilterForm { get; set; }
+        public Category Category { get; set;}
+    }
+}
